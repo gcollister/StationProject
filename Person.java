7ad9d4af9160b395acdc23ce8f40abc5c14c3086
Georgie Collister <gcollister26@students.polytechnic.org>
2025-02-24 08:52:07 -0800

    person getters

--- Person.java
+++ Person.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+
 public class Person {
     
+    private int destination;
+    private static int idGen = 1;
+    private int id;
+
+    public Person(int myDestination){
+    id = idGen;
+    idGen ++;
+    destination = myDestination;
+}
+
+public int getid(){
+    return id;
+}
+
+public int getDestination(){
+    return destination;
+}
+
+public String toString(){
+    return "ID : " + id + " destination : " + destination;
+}
+
+
 }
