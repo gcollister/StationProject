5fa7e0795e36144d88b822cfad0f21aa63cd97dd
Georgie Collister <gcollister26@students.polytechnic.org>
2025-02-24 09:17:25 -0800

    fields and constructors for car

--- Car.java
+++ Car.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
 public class Car {
 
+private int destination;
+private boolean direction;
+private int currentLocation;
+private ArrayList<Person> passengers;
 
+public Car(int myDestination, int myCurrentLocation){
+    destination = myDestination;
+    currentLocation = myCurrentLocation;
+    passengers = new ArrayList<Person>();
+    
+}
 
 }
