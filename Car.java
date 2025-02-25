8274046285d95e39cc24b4e7f2e46ccf271ead83
Georgie Collister <gcollister26@students.polytechnic.org>
2025-02-25 10:01:07 -0800

    move and unload for car; started station

--- Car.java
+++ Car.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 public class Car {
 
 private int destination;
-private boolean direction;
+private boolean direction; //true for right
 private int currentLocation;
 private ArrayList<Person> passengers;
 
@@ -20,15 +20,47 @@ public String toString(){
 public void addPassenger(Person p){
     if (passengers.size() >= 3){
         System.out.println("Error: no room");
-    }else {
+    }else if(p.getDirection() != direction){
+        System.out.println("Error: wrong direction");
+    }else{
         passengers.add(p);
     }
+    }
 
+/** 
+ * Method for handing back a person that is eligible to be dropped off
+ * removed person from car as well
+ * @return one person to be dropped off, null if nobody is available
+ * will only return one person even if multiple availible 
+ * */ 
+
+    public Person unload(){
+    for (int i = 0; i < passengers.size(); i++){
+        Person a = passengers.get(i);
+        if(a.getDestination() == currentLocation){
+            passengers.remove(i);
+            return a;
+        }
+    }
+    return null;
 }
 
 public boolean hasRoom(){
     return passengers.size() < 3;
 
     }
+
+
+public void move(){
+    if (direction == true){
+        currentLocation ++;
+    }else if(direction == false){
+        currentLocation --;
+    }else if (currentLocation == destination){
+        return;
+    }
+    }
 }
+
+
 
--- Station.java
+++ Station.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
 public class Station {
+
+    private int location;
+    private ArrayList<Person> commuters;
+    private static int idGen = 1;
+
+    public Station(){
+        location = idGen;
+        idGen++;
+        commuters = new ArrayList<Person>();
+    }
+
+    public void addCommuter(Person p){
+        commuters.add(p);
+    }
+
+    public 
     
 }
